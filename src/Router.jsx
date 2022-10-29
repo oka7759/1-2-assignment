0fc0c95450964ac81057298dc0ca15624a10cf7a
Youngjun Choi <juni2004c@gmail.com>
2022-10-29 16:31:28 +0900

    [feat] router 세팅 추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,9 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
     </BrowserRouter>
   );
 };
